0175bb1e08f8433d1d14be15f958534d598d6935
sestalin <crjleonardo5@gmail.com>
2016-08-10 10:45:49 -0400

    Modelos Base

---
+++ Bolsa de empleos/Bolsa de empleos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bolsa_de_empleos.Startup))]
+namespace Bolsa_de_empleos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
